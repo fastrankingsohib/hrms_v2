81b2a19b3f0ac25d924f8107240bf228769a69f8
dudi699 <anish@fastranking.tech>
2024-10-22 18:57:42 -0700

    changes made

--- server/controllers/comments_controller.js
+++ server/controllers/comments_controller.js
@@ -52,7 +52,7 @@ try {
         const id = req.params.id
         await prisma.comments.delete({
             where:{
-                comment_id:id
+                comment_id:parseInt(id)
             }
         })
         res.status(200).send({
@@ -64,7 +64,7 @@ try {
  catch (error) {
     console.log(error)
     res.status(200).send({
-        success:true,
+        success:false,
         message:"cannot delete comment"
     })
     
